9dad0bbea10b459249d4647d0047c508ba7dec32
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-08-16 19:54:19 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+
+
 // Write your code here!
+//Remove main div
+const main = document.getElementById('victory');
+main.remove();
+
+
+//Create new heading element with ID atttribute
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Gilbert-Koech is the champion';
+
+main.appendChild(newHeader);
